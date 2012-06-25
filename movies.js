17dd008b2588c7a6a05511f462e6982c451ae1db
Alf Eaton <alf@hubmed.org>
2012-06-25 07:43:38 +0100

    experimenting

--- movies.js
+++ movies.js
@@ -1,42 +1,110 @@
-var movies = $("#movies");
+var RottenTomatoes = {
+    base: "http://api.rottentomatoes.com/api/public/v1.0/",
 
-var base = "http://api.rottentomatoes.com/api/public/v1.0/";
+    defaults: {
+        apikey: "bsmgb5axsjekh4jbwqyt38ak",
+        page_limit: 10,
+        page: 1,
+        country: "uk"
+    },
 
-var defaults = {
-    apikey: "bsmgb5axsjekh4jbwqyt38ak",
-    page_limit: 20,
-    page: 1,
-    country: "uk"
+    get: function(url, params, callback) {
+        params = $.extend({}, this.defaults, params);
+        $.getJSON(url + "?callback=?", params, callback);
+    },
+
+    list: function(type, callback) {
+        this.get(this.base + "lists/movies/" + type + ".json", {}, callback);
+    }
 };
 
-var get = function(url, params, callback) {
-    params = $.extend({}, defaults, params);
-    $.getJSON(url + "?callback=?", params, callback);
+var Movie = function(data) {
+    this.data = data;
+    moviesView.append(this);
+
+    this.set = function(data) {
+        this.data = data;
+        moviesView.replace(this);
+    };
+};
+
+var MovieCollection = function() {
+    var items = [];
+
+    this.fetch = function(type) {
+        this.reset();
+
+        RottenTomatoes.list(type, function(data) {
+            data.movies.forEach(movies.push);
+            moviesView.render();
+            movies.augment();
+        });
+    };
+
+    this.reset = function() {
+        items = [];
+    };
+
+    this.push = function(data) {
+        var movie = new Movie(data);
+        items.push(movie);
+    };
+
+    this.augment = function() {
+        items.forEach(this.details);
+    };
+
+    this.details = function(movie) {
+        RottenTomatoes.get(movie.data.links.self, {}, function(data) {
+            if (data) movie.set(data);
+        });
+    };
+};
+
+var MoviesView = function() {
+    this.el = $("#movies");
+
+    this.init = function() {
+        $(document).on("click", ".movie a", openNewWindow);
+        $(window).bind("hashchange", this.update, this);
+        movies.fetch("opening");
+    }
+
+    this.update = function() {
+        this.reset();
+        movies.fetch(location.hash.replace("#", ""));
+    };
+
+    this.reset = function() {
+        this.el.empty();
+    };
+
+    this.append = function(movie) {
+        var view = new MovieView();
+        view.render().appendTo(this.el);
+    };
+
+    this.replace = function(movie) {
+        var view = new MovieView();
+        movie.el = view.render().replaceAll(movie.el);
+    };
+
+    this.init();
+};
+
+var MovieView = function() {
+    this.template = $("[data-template=movie]").html();
+
+    this.render = function(movie) {
+        this.el = $(Mustache.render(template, movie.data));
+    };
 };
 
 var openNewWindow = function(event) {
     event.preventDefault();
     event.stopPropagation();
-    window.open(event.target.href, "Rotten Tomatoes");
+    window.open(this.href);
 };
 
-var init = function() {
-    movies.empty();
-    
-    var hash = location.hash.replace("#", "") || "opening";
-    
-    get(base + "lists/movies/" + hash + ".json", {}, function showMovies(data) {
-        var template = $("[data-template=movie]").html();
-    
-        data.movies.forEach(function getMovie(movie) {
-            get(movie.links.self, {}, function showMovie(movie) {
-                movies.append(Mustache.render(template, movie));
-            });
-        });
-    });
-};
-    
-movies.on("click", ".movie .title, .movie .link", openNewWindow);
-    
-$(window).bind("hashchange", init);
-init();
+var movies = new MovieCollection;
+var moviesView = new MoviesView;
