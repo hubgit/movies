cc21bc123617a90a8f5a54a2480ceb4d651f335d
Alf Eaton <alf@hubmed.org>
2012-07-05 08:03:08 +0100

    Organise files

--- js/models.js
+++
@@ -1,7 +0,0 @@
-var Models = {};
-
-Models.Movie = Backbone.Model.extend({});
-Models.Movies = Backbone.Collection.extend({ model: Models.Movie });
-
-Models.Link = Backbone.Model.extend({});
-Models.Links = Backbone.Collection.extend({ model: Models.Link });
