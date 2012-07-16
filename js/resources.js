c60cd899ad2ded8171ce020dee43e9bdd91afedd
Alf Eaton <alf@hubmed.org>
2012-07-16 23:08:38 +0100

    Define Views/Collections objects

--- js/resources.js
+++ js/resources.js
@@ -1,114 +1,116 @@
-/*global Backbone, Services, $, window */
+/*global Backbone, Services, Templates, $, window */
 
-var Collections = {}, Views = {}, Templates = {};
+var Collections = {
+    Movies:  Backbone.Collection.extend({
+        sync: function(method, collection, options) {
+            var request;
 
-Collections.Movies = Backbone.Collection.extend({
-    sync: function(method, collection, options) {
-        var request;
+            if(options.url) {
+                request = Services.RottenTomatoes.get(options.url, { queue: false });
+            }
+            else {
+                request = Services.RottenTomatoes.get(Services.RottenTomatoes.listURL(options.type));
+            }
 
-        if(options.url) {
-            request = Services.RottenTomatoes.get(options.url, { queue: false });
+            request.done(options.success);
+        },
+
+        parse: function(data) {
+            this.links = data.links;
+            return data.movies;
         }
-        else {
-            request = Services.RottenTomatoes.get(Services.RottenTomatoes.listURL(options.type));
+    }),
+
+    Links: Backbone.Collection.extend({})
+};
+
+var Views = {
+    Movie: Backbone.View.extend({
+        tagName: "article",
+
+        attributes: {
+            "vocab": "http://schema.org/",
+            "typeof": "Movie"
+        },
+
+        initialize: function() {
+          this.model.on("change", this.render, this);
+        },
+
+        render: function() {
+            this.$el.html(Templates.Movie(this.model.toJSON()));
+            return this;
         }
+    }),
 
-        request.done(options.success);
-    },
+    Movies: Backbone.View.extend({
+        initialize: function() {
+            this.$el.appendTo("body");
+            this.collection.on("reset", this.reset, this);
+            this.collection.on("add", this.add, this);
+        },
 
-    parse: function(data) {
-        this.links = data.links;
-        return data.movies;
-    }
-});
+        events: {
+            "click a": "openNewWindow"
+        },
 
-Collections.Links = Backbone.Collection.extend({});
+        reset: function() {
+            this.$el.empty();
+            this.collection.each(this.add, this);
+        },
 
-Views.Movie = Backbone.View.extend({
-    tagName: "article",
+        add: function(movie) {
+            var view = new Views.Movie({ model: movie });
+            this.$el.append(view.render().el);
+        },
 
-    attributes: {
-        "vocab": "http://schema.org/",
-        "typeof": "Movie"
-    },
+        openNewWindow: function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            window.open(event.currentTarget.href);
+        }
+    }),
 
-    initialize: function() {
-      this.model.on("change", this.render, this);
-    },
+    Pagination: Backbone.View.extend({
+        initialize: function() {
+            this.$el.appendTo("body");
+            this.collection.on("reset", this.reset, this);
+        },
 
-    render: function() {
-        this.$el.html(Templates.Movie(this.model.toJSON()));
-        return this;
-    }
-});
+        reset: function() {
+            this.$el.empty();
+            this.collection.each(this.add, this);
+        },
 
-Views.Movies = Backbone.View.extend({
-    initialize: function() {
-        this.$el.appendTo("body");
-        this.collection.on("reset", this.reset, this);
-        this.collection.on("add", this.add, this);
-    },
+        add: function(links) {
+            var url = links.get("next");
+            if (!url) return;
 
-    events: {
-        "click a": "openNewWindow"
-    },
+            $("<a/>", { href: url, html: "More &darr;", rel: "next" }).appendTo(this.$el);
+        }
+    }),
 
-    reset: function() {
-        this.$el.empty();
-        this.collection.each(this.add, this);
-    },
+    Header: Backbone.View.extend({
+        tagName: "header",
 
-    add: function(movie) {
-        var view = new Views.Movie({ model: movie });
-        this.$el.append(view.render().el);
-    },
+        initialize: function() {
+            this.$el.appendTo("body");
+            this.render();
+        },
 
-    openNewWindow: function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        window.open(event.currentTarget.href);
-    }
-});
+        render: function() {
+            var nav = $("<nav/>").addClass("wrapper");
 
-Views.Pagination = Backbone.View.extend({
-    initialize: function() {
-        this.$el.appendTo("body");
-        this.collection.on("reset", this.reset, this);
-    },
+            var fragments = {
+                "in_theaters": "In Theatres",
+                "opening": "Opening This Week"
+            };
 
-    reset: function() {
-        this.$el.empty();
-        this.collection.each(this.add, this);
-    },
+            $.each(fragments, function(fragment, text) {
+                $("<a/>", { href: "#" + fragment, text: text }).appendTo(nav);
+            });
 
-    add: function(links) {
-        var url = links.get("next");
-        if (!url) return;
-
-        $("<a/>", { href: url, html: "More &darr;", rel: "next" }).appendTo(this.$el);
-    }
-});
-
-Views.Header = Backbone.View.extend({
-    tagName: "header",
-
-    initialize: function() {
-        this.$el.appendTo("body");
-        this.render();
-    },
-
-    render: function() {
-        var nav = $("<nav/>").addClass("wrapper");
-
-        var fragments = {
-            "in_theaters": "In Theatres",
-            "opening": "Opening This Week"
-        };
-
-        $.each(fragments, function(fragment, text) {
-            $("<a/>", { href: "#" + fragment, text: text }).appendTo(nav);
-        });
-
-        this.$el.append(nav);
-    }
-});
+            this.$el.append(nav);
+        }
+    })
+};
